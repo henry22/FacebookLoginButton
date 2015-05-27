e644f6f68a76e3ed70a435e953aa74b7be32bd4c
henry22 <henry811010@gmail.com>
2015-05-27 23:29:05 +0800

    Use Facebook SDK to create a login button.

---
+++ Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Bridging-Header.h
+//  SideProjectForFacebook
+//
+//  Created by Henry on 5/27/15.
+//  Copyright (c) 2015 Henry. All rights reserved.
+//
+
+#ifndef SideProjectForFacebook_Bridging_Header_h
+#define SideProjectForFacebook_Bridging_Header_h
+
+//#import <FBSDKCoreKit/FBSDKCoreKit.h>
+//#import <FBSDKLoginKit/FBSDKLoginKit.h>
+
+
+#endif
